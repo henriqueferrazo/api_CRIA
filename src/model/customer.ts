13bc2690c2230c594fe2d9b618bf8c2c6cbc00cc
Henrique Ferraz de Oliveira <henrique.oliveira@calindra.com.br>
2022-09-09 14:20:52 -0300

    modelando dados do endereco final

---
+++ src/model/customer.ts
@@ -0,0 +1,17 @@
+
+
+class Customer {
+
+    id: number | undefined
+    contactName: string | undefined;
+    address: string | undefined;
+    number: string | undefined;
+    city:string | undefined;
+    state: string | undefined;
+    zipCode: string | undefined;
+    latitude: 0 | undefined;
+    longitude: 0 | undefined;
+    email: string | undefined
+
+    
+}
