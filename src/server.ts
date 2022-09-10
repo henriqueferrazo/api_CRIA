21b3856e7243502b5f3fffffd7b7d6d74e78c525
Henrique Ferraz de Oliveira <henrique.oliveira@calindra.com.br>
2022-09-10 15:30:22 -0300

    resolvendo erro de router

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,23 @@
-import  express from 'express'
-import {Router, Request, Response}  from "express";
+import  express, {Request, Response} from 'express'
 import  *  as  dotenv  from  'dotenv'
-import AdressRouter from './postServiceOrder/router/Router';
 import DataBaseAddress from './postServiceOrder/repository/DataBaseAddress';
 import AdressControler from './postServiceOrder/controller/AddressController';
+import AddressRouter from './postServiceOrder/router/Router';
 
 
 dotenv.config({debug: true})
 const port = process.env.PORT
 const app = express();
-const route = Router();
 const dataBase = new DataBaseAddress()
 const addressController = new AdressControler()
 
 dataBase.connectToDataBase()
 app.use(express.json())
-app.use(route)
+app.use('/address',AddressRouter)
 
-route.get('/', (req:Request, res:Response) =>{
+app.get('/', (req:Request, res:Response) =>{
     res.json("olá mundo");
 })
-route.post('/serviceOrder', addressController.ListenServiceOrden)
-
 
 
 app.listen(port, () => {
